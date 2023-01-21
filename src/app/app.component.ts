dcf7fbad14af7e966cea681ad8f1081022af5d74
Dennis Hilgert <d.hilgert@laserhub.com>
2023-01-21 16:11:59 +0100

    Added experimental error handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core'
 import { IdentityService } from './core/services/identity.service'
 import { SocketService } from './core/services/socket.service'
 import { StateService } from './core/services/state.service'
+import { ToastService, ToastType } from './core/services/toast.service'
 import { IIdentity } from './models/identity.model'
+import { IToast } from './models/toast.model'
 
 @Component({
   selector: 'app-root',
@@ -17,12 +19,25 @@ export class AppComponent implements OnInit {
   constructor(
     private stateService: StateService,
     private identityService: IdentityService,
-    private socketService: SocketService
+    private socketService: SocketService,
+    private toastService: ToastService
   ) {}
 
   ngOnInit(): void {
     this.identityService.init().then((identity: IIdentity) => {
       this.socketService.init(identity)
     })
+
+    const errorHandler = (error: ErrorEvent) => {
+      const toast: IToast = {
+        title: 'An unhandled error has occurred',
+        content: error.message,
+        type: ToastType.ERROR
+      }
+      this.toastService.showToast(toast)
+    }
+
+    window.addEventListener('error', errorHandler)
+    window.addEventListener('unhandledrejection', errorHandler)
   }
 }
